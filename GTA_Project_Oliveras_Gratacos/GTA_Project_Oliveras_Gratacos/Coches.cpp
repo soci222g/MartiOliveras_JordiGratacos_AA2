048eff5eb46ef264296730135b86e6c9e8ce1941
m4rtiop <marti.oliveras@enti.cat>
2025-06-06 12:44:50 +0200

    iuupioi

--- GTA_Project_Oliveras_Gratacos/GTA_Project_Oliveras_Gratacos/Coches.cpp
+++ GTA_Project_Oliveras_Gratacos/GTA_Project_Oliveras_Gratacos/Coches.cpp
@@ -4,11 +4,26 @@
 Coches::Coches()
 {
 	LecturaArchivo archivo;
-	nCarI1 = archivo.GetnCoche1();
-	nCarI2 = archivo.GetnCoche2();
-	nCarI3 = archivo.GetnCoche3();
+	const nCarI1 = archivo.GetnCoche1();
+	const nCarI2 = archivo.GetnCoche2();
+	const nCarI3 = archivo.GetnCoche3();
 
+	Coche cocheI1[nCarI1];
+	Coche cocheI2[nCarI2];
+	Coche cocheI3[nCarI3];
 
+	for (int i = 0; i < nCarI1; i++) {
+		cocheI1[i].Position.X = (rand() % (leftLimit1 - 2)) + 1;
+		cocheI1[i].Position.Y = (rand() % (Height - 2)) + 1;
+	}
+	for (int i = 0; i < nCarI2; i++) {
+		cocheI2[i].Position.X = (rand() % (leftLimit1 - 2)) + 1;
+		cocheI2[i].Position.Y = (rand() % (Height - 2)) + 1;
+	}
+	for (int i = 0; i < nCarI3; i++) {
+		cocheI3[i].Position.X = (rand() % (leftLimit1 - 2)) + 1;
+		cocheI3[i].Position.Y = (rand() % (Height - 2)) + 1;
+	}
 }
 
 
