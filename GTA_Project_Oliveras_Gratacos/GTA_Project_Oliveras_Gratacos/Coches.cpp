697f2e9c45de15ff2f937a96254c918d35ab10e6
m4rtiop <marti.oliveras@enti.cat>
2025-06-06 13:14:29 +0200

    fiñauuuuu

--- GTA_Project_Oliveras_Gratacos/GTA_Project_Oliveras_Gratacos/Coches.cpp
+++ GTA_Project_Oliveras_Gratacos/GTA_Project_Oliveras_Gratacos/Coches.cpp
@@ -2,27 +2,18 @@
 #include"lecturaArchivo.h"
 
 Coches::Coches()
-{
-	LecturaArchivo archivo;
-	const nCarI1 = archivo.GetnCoche1();
-	const nCarI2 = archivo.GetnCoche2();
-	const nCarI3 = archivo.GetnCoche3();
+{	
+	plInCar = false;
+	bool aux = false;
+	while (!aux)
+	{
+		Position.X = (rand() % (leftLimit1 - 2)) + 1;
+		Position.Y = (rand() % (Height - 2)) + 1;
 
-	Coche cocheI1[nCarI1];
-	Coche cocheI2[nCarI2];
-	Coche cocheI3[nCarI3];
-
-	for (int i = 0; i < nCarI1; i++) {
-		cocheI1[i].Position.X = (rand() % (leftLimit1 - 2)) + 1;
-		cocheI1[i].Position.Y = (rand() % (Height - 2)) + 1;
-	}
-	for (int i = 0; i < nCarI2; i++) {
-		cocheI2[i].Position.X = (rand() % (leftLimit1 - 2)) + 1;
-		cocheI2[i].Position.Y = (rand() % (Height - 2)) + 1;
-	}
-	for (int i = 0; i < nCarI3; i++) {
-		cocheI3[i].Position.X = (rand() % (leftLimit1 - 2)) + 1;
-		cocheI3[i].Position.Y = (rand() % (Height - 2)) + 1;
+		if (mapa.getCella(Position.X, Position.Y) == Cella::VACIA)
+		{
+			aux = true;
+		}
 	}
 }
 
